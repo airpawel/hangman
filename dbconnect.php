04868dad500d62ac1c54259670b4026919d67ab4
airpawel <kwolek0@gmail.com>
2017-04-10 22:37:58 +0200

    Delete of dbconnect.php
     It was replaced with dbconnect_local.php

--- dbconnect.php
+++
@@ -1,7 +0,0 @@
-<?php
-    	$host="localhost";		// Nazwa hosta
-    	$user="root"; 				// Nazwa użytkownika - domyślnie: root
-    	$dbpassword=""; 			// Haslo do bazy
-    	$database="hangmanpasswords";	 	// Nazwa bazy
-    	$table="passwords" 			// Nazwa tabeli
-?>
